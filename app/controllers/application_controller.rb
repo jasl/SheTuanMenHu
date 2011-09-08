0fc37688a11875cd70a57453dd6bc54b1cbee819
jasl <jasl9187@hotmail.com>
2011-09-09 03:07:29 +0800

    some files missed in last commit- -

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery
+
+  protected
+
+  def ckeditor_filebrowser_scope(options = {})
+    super
+    {
+        :assetable_id => current_user.id,
+        :assetable_type => 'User'
+    }.merge(options)
+  end
 end
