6e7023df9a3f6b7646ca439b9bc610778c0062f4
jasl <jasl9187@hotmail.com>
2011-10-28 12:30:04 +0800

    fix group model's constraint.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,16 @@
 class Group < ActiveRecord::Base
-  has_many :contents
+  has_many :contents, :dependent => :destroy
+  has_many :members, :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => true
   validates :permalink, :presence => true, :uniqueness => true
   validates :school, :presence => true
+
+  name_regex = /[^x00-xff]/u
+  permalink_regex = /^[a-z]{4,}/i
+
+  validates :permalink, :format => { :with => permalink_regex }
+  validates :name, :format => { :with => name_regex }
+  validates :school, :format => { :with => name_regex }
 
 end
